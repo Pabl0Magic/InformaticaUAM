0e07199da003fa0e9f85226e9cafc315fbc1c0d5
Pablo <pabloalmarzamarques@gmail.com>
2023-09-26 16:21:35 +0200

    Add files via upload

---
+++ 3/PSI/build.sh
@@ -0,0 +1,17 @@
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+# python3 manage.py createsuperuser
+
+export DJANGO_SUPERUSER_PASSWORD=alumnodb
+export DJANGO_SUPERUSER_USERNAME=alumnodb
+export DJANGO_SUPERUSER_EMAIL=alumnodb@uam.es
+
+# python3 populate_catalog.py
+# python3 manage.py createsuperuser --noinput
